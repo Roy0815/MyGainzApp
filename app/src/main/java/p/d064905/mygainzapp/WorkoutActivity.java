f26b3565a6334bf1e0b24273085dc88191d314be
mhoppe <michaelmattern93@gmx.net>
2018-07-01 12:17:49 +0200

    Erstellung und Implementierung des PopUp-Menüs, Fixen der ActionBar

--- app/src/main/java/p/d064905/mygainzapp/WorkoutActivity.java
+++ app/src/main/java/p/d064905/mygainzapp/WorkoutActivity.java
@@ -1,8 +1,14 @@
 package p.d064905.mygainzapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import java.io.UnsupportedEncodingException;
@@ -20,17 +26,40 @@ public class WorkoutActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
 
-        getActionBar().setTitle("Workout");
-
         cancelworkoutbutton = findViewById(R.id.workoutabbrechenButton);
         endworkoutbutton = findViewById(R.id.workoutbeendenButton);
         textview = findViewById(R.id.textView2);
         aktualisieren();
-
     }
+
+    //PopUp Menü in Action Bar reinladen als 3 Menüpunkte oben rechts
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.popupmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Wir prüfen, ob Menü-Element mit der ID "overview"
+        // ausgewählt wurde und geben eine Meldung aus
+        int id = item.getItemId();
+        if (id == R.id.overview) {
+            changescreen();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     public void aktualisieren() {
         workoutsync = new WorkoutSync(textview);
         workoutsync.execute("https://mygainzapp.appspot.com/gainzapp/exercises?workout=5630742793027584");
+    }
+
+    public void changescreen() {
+        Intent intent0 = new Intent(this, MainActivity.class);
+        startActivity(intent0);
     }
 
 }
