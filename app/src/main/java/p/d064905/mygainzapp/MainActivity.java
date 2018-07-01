b6c7a69781999a70481f2d931e58bc783f9a59fd
Hendrik Madeya <hennandroid@gmail.com>
2018-07-01 12:35:30 +0200

    Neue Logik für PlanSync

--- app/src/main/java/p/d064905/mygainzapp/MainActivity.java
+++ app/src/main/java/p/d064905/mygainzapp/MainActivity.java
@@ -3,20 +3,18 @@ package p.d064905.mygainzapp;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.appwidget.AppWidgetManager;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.PopupMenu;
 import android.widget.ScrollView;
 import android.widget.Toolbar;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 
 import static android.support.v4.view.WindowCompat.FEATURE_ACTION_BAR;
@@ -26,6 +24,7 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
     ListView wActive;
     ArrayList<Plan>  mArray;
     ArrayAdapter<Plan>  mAdapter;
+    PlanSync Ps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,27 +37,17 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
         mAdapter= new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,mArray);
         wActive.setAdapter(mAdapter);
         wActive.setOnItemClickListener(this);
-        onRefresh();
+
 
     }
 
-    //PopUp Menü in Action Bar reinladen als 3 Menüpunkte oben rechts
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.popupmenu, menu);
-        return true;
-    }
-
+    public void onRefresh() {
+        Ps = new PlanSync(mArray);
+        Ps.execute("https://mygainzapp.appspot.com/gainzapp/plans");}
 
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         //onclick Workout
-    }
-
-    public void changescreen(View aView) {
-        Intent intent2 = new Intent(this, WorkoutActivity.class);
-        startActivity(intent2);
     }
 }
